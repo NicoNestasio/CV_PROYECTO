65565aa0eba1ae83609e9bcb4739472088cc17a7
Nicolas <niconesta66@gmail.com>
2023-01-25 06:58:43 -0300

    Uso de API user generator e información añadida en las secciones.

--- Proyecto/function.js
+++ Proyecto/function.js
@@ -1,3 +1,21 @@
+
 fetch('https://randomuser.me/api/')
       .then(response => response.json())
-      .then(json => console.log(json))
+      .then(persona => mostrarData(persona))
+function gen(genero){
+      var traduccion;
+      if (genero=='female'){
+                  traduccion='femenino';
+      } else {
+                  traduccion='masculino';
+      }
+      return traduccion
+}
+const mostrarData = (persona) =>{
+      console.log(persona)
+      var genero = gen(persona.results[0].gender);
+      document.getElementById('nombre').innerHTML= persona.results[0].name.first;
+      document.getElementById('apellido').innerHTML= persona.results[0].name.last;
+      document.getElementById('imagen').src= persona.results[0].picture.large;
+      document.getElementById('infop').innerHTML= "<li>Edad: " + persona.results[0].dob.age + " años</li>" + "<li>Género: " + genero + "</li>" + "<li>Nacionalidad: " + persona.results[0].nat + "</li>" + "<li>Residencia: " + persona.results[0].location.city + ", " + persona.results[0].location.state + ", " + persona.results[0].location.country + "</li>" +  "<li>ID: " + persona.results[0].id.name + " " + persona.results[0].id.value + "</li>"
+}
