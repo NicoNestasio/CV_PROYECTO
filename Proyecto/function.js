169f299bd3a938ae1555fbd57200fc9835c8f65d
Nicolas <niconesta66@gmail.com>
2023-01-26 06:27:30 -0300

    Finalizado el contenido del body, comienzo de botón de cambio de color

--- Proyecto/function.js
+++ Proyecto/function.js
@@ -17,5 +17,12 @@ const mostrarData = (persona) =>{
       document.getElementById('nombre').innerHTML= persona.results[0].name.first;
       document.getElementById('apellido').innerHTML= persona.results[0].name.last;
       document.getElementById('imagen').src= persona.results[0].picture.large;
+      document.getElementById('email').innerHTML= persona.results[0].email;
+      document.getElementById('cel').innerHTML= persona.results[0].cell;
       document.getElementById('infop').innerHTML= "<li>Edad: " + persona.results[0].dob.age + " años</li>" + "<li>Género: " + genero + "</li>" + "<li>Nacionalidad: " + persona.results[0].nat + "</li>" + "<li>Residencia: " + persona.results[0].location.city + ", " + persona.results[0].location.state + ", " + persona.results[0].location.country + "</li>" +  "<li>ID: " + persona.results[0].id.name + " " + persona.results[0].id.value + "</li>"
+}
+
+document.getElementById('boton').onclick =function(){
+
+      document.body.style.backgroundColor = '#EBEBEB' ;
 }
